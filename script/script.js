75efff8d8f09821edf40af23ae80ebee785e63c9
jappejappe <gabrieljappemoura95071@gmail.com>
2024-12-16 22:33:11 -0300

    style and html fixed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-const tab = document.getElementById("tab");
+const cells = document.querySelectorAll('.celula');
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+    const position = cell.dataset.pos; // Obtém o valor do atributo data-pos
+    console.log(`Você clicou na posição: ${position}`);
+    });
+});
